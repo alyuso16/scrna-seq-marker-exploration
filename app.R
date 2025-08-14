bd103ef537f39aff39836487562dc0150feaed53
Alex Song <>
2025-08-13 22:23:39 -0400

    added upload message to indicate filetype

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(shinythemes)
 library(shinyjs)
+library(bslib)
 
 source("R/preprocessing.R")
 source("R/plots.R")
@@ -10,19 +11,29 @@ ui = fluidPage(theme = shinytheme("cosmo"),
   navbarPage("scRNA-seq",
     tabPanel("Tab 1",
       useShinyjs(),
-      sidebarPanel(
-        fileInput("file", "File upload:", multiple = FALSE, accept = c(".h5, .rds")),
-        actionButton("run", "Run", icon = icon("play")),
 
-        textInput("save_name", "Save Seurat object as:", value = "seurat_obj"),
+      sidebarPanel(
+        div(
+          style = "margin-bottom: 20px;",
+          fileInput("file", "File upload:", multiple = FALSE, accept = c(".h5, .rds")),
+          uiOutput("upload_msg"),
+        ),
+        div(
+          style = "margin-bottom: 20px;",
+          actionButton("run", "Run", icon = icon("play")),
+        ),
+
+        textInput("save_name", "Save Seurat object as:"),
         downloadButton("download_object", "Download Seurat Object"),
       ),
+
       mainPanel(
         fluidRow(
           column(
             width = 9,
             plotOutput("plot")
           ),
+
           column(
             width = 3,
             uiOutput("plot_select"),
@@ -56,6 +67,24 @@ server <- function(input, output) {
     } else {
       run_preprocessing(input$file)
     }
+  })
+
+  output$upload_msg <- renderUI({
+    req(input$file)
+    filetype <- get_filetype(input$file)
+
+    tags$span(
+      style = "color: green;",
+      paste(
+        "Uploaded ", filetype, " file,",
+        if (filetype == "rds") {
+          "run to skip preprocessing..."
+        } else if (filetype == "h5") {
+          "run to begin preprocessing..."
+        }
+      )
+    )
+    
   })
 
   output$download_object <- downloadHandler(
