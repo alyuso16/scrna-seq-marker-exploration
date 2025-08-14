246553280567c0b72945660e63228ece5970f897
Alex Song <>
2025-08-13 20:36:49 -0400

    cleanup

--- app.R
+++ app.R
@@ -6,84 +6,85 @@ source("R/preprocessing.R")
 source("R/plots.R")
 source("R/validation.R")
 
-shinyApp(
-  ui = fluidPage(theme = shinytheme("cosmo"),
-    navbarPage("scRNa-seq",
-      tabPanel("Tab 1",
-        useShinyjs(),
-        sidebarPanel(
-          fileInput("file", "File upload:", multiple = FALSE, accept = c(".h5, .rds")),
-          actionButton("run", "Run", icon = icon("play"))
-        ),
-        mainPanel(
-          fluidRow(
-            column(
-              width = 9,
-              plotOutput("plot")
-            ),
-            column(
-              width = 3,
-              uiOutput("plot_select"),
-              uiOutput("marker_select")
-            )
+ui = fluidPage(theme = shinytheme("cosmo"),
+  navbarPage("scRNa-seq",
+    tabPanel("Tab 1",
+      useShinyjs(),
+      sidebarPanel(
+        fileInput("file", "File upload:", multiple = FALSE, accept = c(".h5, .rds")),
+        actionButton("run", "Run", icon = icon("play"))
+      ),
+      mainPanel(
+        fluidRow(
+          column(
+            width = 9,
+            plotOutput("plot")
+          ),
+          column(
+            width = 3,
+            uiOutput("plot_select"),
+            uiOutput("marker_select")
           )
         )
       )
     )
-  ),
-  server <- function(input, output) {
-    options(shiny.maxRequestSize = 1000 * 1024^2)
-
-    disable("run")
-
-    observeEvent(input$file, {
-      if (!is.null(input$file)) {
-        enable("run")
-      } else {
-        disable("run")
-      }
-    })
-
-    seurat_obj <- eventReactive(input$run, {
-      req(input$file)
-      filetype <- get_filetype(input$file)
-
-      if (filetype == "rds") {
-        readRDS(input$file$datapath)
-      } else {
-        run_preprocessing(input$file)
-      }
-    })
-
-    output$plot <- renderPlot({
-      req(seurat_obj(), input$selected_plot)
-
-      if (input$selected_plot == "UMAP") {
-        plot_umap(seurat_obj())
-      } else if (input$selected_plot == "Features") {
-        req(input$selected_marker)
-        plot_feature(seurat_obj(), marker = input$selected_marker)
-      }
-    })
-
-    output$marker_select <- renderUI({
-      req(seurat_obj())
-      selectizeInput(
-        "selected_marker",
-        "Select marker gene:",
-        choices = get_features(seurat_obj()),
-        selected = get_features(seurat_obj())[1]
-      )
-    })
-
-    output$plot_select <- renderUI({
-      req(seurat_obj())
-      selectizeInput(
-        "selected_plot",
-        "Select plot to display:",
-        choices = c("UMAP", "Features"),
-        selected = "UMAP"
-      )
-    })
-  }
+  )
 )
+
+server <- function(input, output) {
+  options(shiny.maxRequestSize = 1000 * 1024^2)
+
+  disable("run")
+
+  observeEvent(input$file, {
+    if (!is.null(input$file)) {
+      enable("run")
+    } else {
+      disable("run")
+    }
+  })
+
+  seurat_obj <- eventReactive(input$run, {
+    req(input$file)
+    filetype <- get_filetype(input$file)
+
+    if (filetype == "rds") {
+      readRDS(input$file$datapath)
+    } else {
+      run_preprocessing(input$file)
+    }
+  })
+
+  output$plot <- renderPlot({
+    req(seurat_obj(), input$selected_plot)
+
+    if (input$selected_plot == "UMAP") {
+      plot_umap(seurat_obj())
+    } else if (input$selected_plot == "Features") {
+      req(input$selected_marker)
+      plot_feature(seurat_obj(), marker = input$selected_marker)
+    }
+  })
+
+  output$marker_select <- renderUI({
+    req(seurat_obj())
+    selectizeInput(
+      "selected_marker",
+      "Select marker gene:",
+      choices = get_features(seurat_obj()),
+      selected = get_features(seurat_obj())[1]
+    )
+  })
+
+  output$plot_select <- renderUI({
+    req(seurat_obj())
+    selectizeInput(
+      "selected_plot",
+      "Select plot to display:",
+      choices = c("UMAP", "Features"),
+      selected = "UMAP"
+    )
+  })
+}
+
+shinyApp(ui, server)
